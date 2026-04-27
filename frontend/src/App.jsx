bcfb014dbb01bb2210e5c06474d7a557b23c38b3
Rahul Kumar Sahu <rahulssahu1116@gmail.com>
2026-04-27 19:24:26 +0530

    created folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl text-blue-500">
+      Tailwind is working 🚀
+    </h1>
+    </>
+  )
+}
+
+export default App
